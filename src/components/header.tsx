2f01ea4f6abe88c1559c780150c5c6d9ab667865
Brenobn <brenobragabn@gmail.com>
2025-08-26 14:55:29 -0300

    Importing some icons and adding some styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,27 @@
+import { PiSparkle, PiSun, PiBell } from 'react-icons/pi';
+
 export function Header() {
   return (
-    <header className="h-16 w-full border-b-background-700 border-b-[0.8px] border-b-[rgb(229,231,235)] flex justify-between items-center px-[5%]">
+    <header className="h-16 w-full shadow-sm border-b-background-700 border-b-[0.8px] border-b-[rgb(229,231,235)] flex justify-between items-center px-[4%]">
       
+      <div className="flex items-center ">
+        <h2 className="text-lg font-semibold text-gray-300">
+          Sistema de Gestão - PROVER
+        </h2>
+      </div>
+      <div className="flex items-center space-x-4">
+        
+        <span className="flex items-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 gap-1 border-transparent bg-gray-75">
+          <PiSparkle />
+          IA Ativa
+        </span>
+
+        <PiSun />
+
+        <PiBell />
+
+      </div>
+
     </header>
   );
 }
