74f5095d9c1c7737da790aaab234f9c94b6a4ac0
Brenobn <brenobragabn@gmail.com>
2025-08-26 10:32:33 -0300

    Starting to build Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="h-16 w-full border-b-background-700 border-b-[0.8px] border-b-[rgb(229,231,235)] flex justify-between items-center px-[5%]">
+      
+    </header>
+  );
+}
