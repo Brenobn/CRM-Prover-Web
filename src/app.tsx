486979d0a2c3a98fc41502b72ba67b5cd95d7145
Brenobn <brenobragabn@gmail.com>
2025-08-29 09:21:43 -0300

    Removing the nav from the app file and creating a component for it

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,7 @@
 import { Header } from './components/header';
 
-import { LuChartColumn, LuUsers, LuFileText } from 'react-icons/lu';
-import { PiSparkle } from 'react-icons/pi';
-import { IoSettingsOutline } from 'react-icons/io5';
 import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from 'react-icons/md';
+import { Sidebar } from './components/Sidebar';
 
  
 export function App() {
@@ -23,34 +21,7 @@ export function App() {
       <Header />
 
       {/* Menu */}
-      <nav className="flex items-center flex-col bg-background-900 text-center col-span-1 row-span-2 border-r-[0.8px] border-r-[rgb(229,231,235)]">
-        <div className='flex items-start flex-col mt-6'>
-          <a className='w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-2 text-sm font-medium text-gray-100 transition-colors hover:text-gray-200 hover:bg-gray-75'>
-            <LuChartColumn size={20} className='text-gray-100 transition-colors group-hover:text-gray-200'/>
-            Dashboard
-          </a>
-          <a className='w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-2 text-sm font-medium text-gray-100 transition-colors hover:text-gray-200 hover:bg-gray-75'>
-            <LuChartColumn size={20} className='text-gray-100 transition-colors group-hover:text-gray-200'/>
-            Dashboard Avançado
-          </a>
-          <a className='w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-2 text-sm font-medium text-gray-100 transition-colors hover:text-gray-200 hover:bg-gray-75'>
-            <LuUsers size={20} className='text-gray-100 transition-colors group-hover:text-gray-200'/>
-            Leads
-          </a>
-          <a className='w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-2 text-sm font-medium text-gray-100 transition-colors hover:text-gray-200 hover:bg-gray-75'>
-            <LuFileText size={20} className='text-gray-100 transition-colors group-hover:text-gray-200'/>
-            Dados
-          </a>
-          <a className='w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-2 text-sm font-medium text-gray-100 transition-colors hover:text-gray-200 hover:bg-gray-75'>
-            <PiSparkle size={20} className='text-gray-100 transition-colors group-hover:text-gray-200'/>
-            Diagnóstico
-          </a>
-          <a className='w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-2 text-sm font-medium text-gray-100 transition-colors hover:text-gray-200 hover:bg-gray-75'>
-            <IoSettingsOutline size={20} className='text-gray-100 transition-colors group-hover:text-gray-200'/>
-            Configurações
-          </a>
-        </div>
-      </nav>
+      <Sidebar />
 
       {/* Search */}
       <div className="pt-16 px-16 col-span-1 row-span-1">
