0657bffed87db63c5538ef1b1b495bf29843fecf
Brenobn <brenobragabn@gmail.com>
2025-09-05 16:55:06 -0300

    Importing tailwind-variants to better organize styles

--- src/components/Sidebar/MainNavigation/Sublink.tsx
+++ src/components/Sidebar/MainNavigation/Sublink.tsx
@@ -1,13 +1,28 @@
 import type { ReactNode } from "react";
+import { tv } from "tailwind-variants";
+
+const sublinkStyles = tv({
+  base: 'px-3 py-2 rounded-lg cursor-pointer transition-colors duration-300 ease-in-out text-left text-gray-300 text-sm hover:text-gray-200 hover:bg-gray-75 dark:text-white dark:hover:bg-gray-100 dark:hover:text-white',
+  variants: {
+    isActive: {
+      true: 'bg-gray-75 dark:bg-gray-100 font-semibold',
+      false: '',
+    }
+  },
+  defaultVariants: {
+    isActive: false,
+  }
+});
 
 export interface SublinkProps {
   href: string;
   children: ReactNode; 
+  isActive: boolean;
 }
 
-export function Sublink({ href, children }: SublinkProps) {
+export function Sublink({ href, children, isActive }: SublinkProps) {
   return(
-    <a href={href} className="px-3 py-2 rounded-lg cursor-pointer hover:text-gray-200 hover:bg-gray-75 dark:text-white dark:hover:bg-gray-100 dark:hover:text-white transition-colors duration-300 ease-in-out text-left text-gray-300 text-sm">
+    <a href={href} className={sublinkStyles({ isActive })}>
       {children}
     </a>
   )
