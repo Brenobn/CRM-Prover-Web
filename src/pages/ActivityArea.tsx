f94b8556e97f5255a5c5f78891ff6d3f99d449ee
Brenobn <brenobragabn@gmail.com>
2025-09-08 14:51:25 -0300

    Adding some styles to the page

--- src/pages/ActivityArea.tsx
+++ src/pages/ActivityArea.tsx
@@ -1,9 +1,12 @@
-import { Header } from "../components/header";
-
 export function ActitvityArea() {
   return(
-    <div>
-      <Header />
-    </div>
+    <section>
+      <header className="flex items-center justify-between mt-5">
+        <h1 className="text-2xl font-normal text-gray-150 dark:text-gray-75">Área de Atuação</h1>
+        <div className="inline-flex text-center gap-1">
+          <a href="#" className="text-blue-500 font-light">Home <span className="text-gray-90">/ Área de Atuação</span></a>
+        </div>
+      </header>
+    </section>
   );
 }
