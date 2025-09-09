37478108d4cd6230bd6a2a86bae6720b60ca06df
Brenobn <brenobragabn@gmail.com>
2025-09-09 15:37:42 -0300

    importing DataTableToolbar, DataTablePagination and Button. Setting some styles

--- src/pages/ActivityArea.tsx
+++ src/pages/ActivityArea.tsx
@@ -3,7 +3,11 @@ import type { ColumnFiltersState } from '@tanstack/react-table'
 import { flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, getFilteredRowModel } from '@tanstack/react-table'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../components/ui/table"
 
+import { DataTableToolbar } from '../components/DataTableToolbar'
+import { DataTablePagination } from '../components/DataTablePagination'
+
 import { columns, data } from './ActivityAreaColumns'
+import { Button } from '../components/ui/button'
  
 export function ActitvityArea() {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
@@ -21,15 +25,21 @@ export function ActitvityArea() {
   });
 
   return(
-    <section className="container mx-auto py-10">
-      <div className="flex items-center justify-between mt-5">
+    <section className="flex flex-col h-full container gap-4 py-4">
+      
+      <div className="flex items-center justify-between shrink-0">
         <h1 className="text-2xl font-normal text-gray-150 dark:text-gray-75 -tracking-tight">Área de Atuação</h1>
         <div className="inline-flex text-center gap-1">
           <a href="#" className="text-blue-500 font-light">Home <span className="text-gray-90">/ Área de Atuação</span></a>
         </div>
+        <Button>Adicionar Nova Área</Button>
       </div>
 
-      <div className="rounded-md border">
+      <div className='shrink-0'>
+        <DataTableToolbar table={table} />
+      </div>
+
+      <div className="flex-1 rounded-md border overflow-y-auto">
         <Table>
           <TableHeader>
             {table.getLeftHeaderGroups().map((headerGroup) => (
@@ -69,6 +79,7 @@ export function ActitvityArea() {
           </TableBody>
         </Table>
       </div>
+      <DataTablePagination table={table} />
     </section>
   );
 }
