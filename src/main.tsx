47bd6c194f71ec1f1904c6a6d001f2151984cd89
Brenobn <brenobragabn@gmail.com>
2025-09-10 16:51:03 -0300

    Iporting the SiginIn, SiginUp,  ProtectedRoute and AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,23 +4,45 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 import { App } from './app'
+import { SiginIn } from './pages/SiginIn'
+import { SiginUp } from './pages/SiginUp'
 import { ActitvityArea } from './pages/ActivityArea'
+import { ProtectedRoute } from './routes/ProtectedRoute'
+import { AuthProvider } from './hooks/auth'
 
 const router = createBrowserRouter([
+  {
+    path: '/siginin',
+    element: <SiginIn />,
+  },
+  {
+    path: '/siginup',
+    element: <SiginUp />
+  },
   {
     path: '/',
     element: <App />,
     children: [
       {
-        path: '/configuracoes/area-de-atuacao',
+        path: 'configuracoes/area-de-atuacao',
         element: <ActitvityArea />,
       },
-    ]
+    ],
+  },
+  
+  {
+    path: '/',
+    element: <ProtectedRoute />,
+    children: [
+      
+    ],
   },
 ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
