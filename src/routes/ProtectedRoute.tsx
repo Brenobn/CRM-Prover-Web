84ddbabefda95f45f9a1d2394a6dc2ff9e22befb
Brenobn <brenobragabn@gmail.com>
2025-09-10 09:44:42 -0300

    Creating a routes with ProtectedRoute

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../hooks/auth"
+
+export function ProtectedRoute() {
+  const { user } = useAuth()
+
+  if(!user) {
+    return <Navigate to="/login" />
+  }
+
+  return <Outlet />
+}
