8244fbe7c6741b176272c5f19c68865bb7c954b0
Brenobn <brenobragabn@gmail.com>
2025-09-11 17:45:47 -0300

    Making some changes and importing some dependencies too

--- src/pages/ActivityArea.tsx
+++ src/pages/ActivityArea.tsx
@@ -1,16 +1,32 @@
 import { useState } from 'react'
 import type { ColumnFiltersState } from '@tanstack/react-table'
-import { flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, getFilteredRowModel } from '@tanstack/react-table'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../components/ui/table"
-
+import { 
+  flexRender, 
+  getCoreRowModel, 
+  useReactTable, 
+  getPaginationRowModel, 
+  getFilteredRowModel 
+} from '@tanstack/react-table'
+import { 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableHead, 
+  TableHeader, 
+  TableRow } from "../components/ui/table"
+import { Button } from '../components/ui/button'
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '../components/ui/sheet'
 import { DataTableToolbar } from '../components/DataTableToolbar'
 import { DataTablePagination } from '../components/DataTablePagination'
+import { AreaDeAruacaoForm } from '../components/ActivityAreaForm'
+import type { AreaDeAtuação } from './ActivityAreaColumns'
 
-import { columns, data } from './ActivityAreaColumns'
-import { Button } from '../components/ui/button'
+import { columns, data as initialData } from './ActivityAreaColumns'
  
 export function ActitvityArea() {
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [data, setData] = useState<AreaDeAtuação[]>(initialData)
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [isSheetOpen, setIsSheetOpen] = useState(false)
 
   const table = useReactTable({
     data,
@@ -24,16 +40,49 @@ export function ActitvityArea() {
     },
   });
 
+  function handleFormSubmit(values: { descricao: string; ativo: boolean }) {
+    console.log("Adicionando nova área:", values)
+
+    const newArea: AreaDeAtuação = {
+      id: (data.length + 1).toString(),
+      descrição: values.descricao,
+      ativo: values.ativo,
+    }
+
+    setData(currentData => [...currentData, newArea])
+
+    setIsSheetOpen(false)
+  }
+
   return(
-    <section className="flex flex-col h-full container gap-4 py-4">
+    <section className="h-full flex flex-col container gap-4">
       
-      <div className="flex items-center justify-between shrink-0">
-        <h1 className="text-2xl font-normal text-gray-150 dark:text-gray-75 -tracking-tight">Área de Atuação</h1>
-        <div className="inline-flex text-center gap-1">
-          <a href="#" className="text-blue-500 font-light">Home <span className="text-gray-90">/ Área de Atuação</span></a>
+      <header className="flex items-center justify-between shrink-0">
+        <div className='space-y-1.5'>
+          <h1 className="text-2xl font-normal text-gray-150 dark:text-gray-75 -tracking-tight">Área de Atuação</h1>
+          <div className="text-sm text-muted-foreground">
+            <a href="#" className="text-blue-500 font-light">Home</a>
+            <span className="text-gray-90">/ Área de Atuação</span>
+          </div>
         </div>
-        <Button>Adicionar Nova Área</Button>
-      </div>
+
+          <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+            <SheetTrigger asChild>
+              <Button>Adicionar Nova Área</Button>
+            </SheetTrigger>
+            <SheetContent className='flex flex-col sm:max-w-md p-0 shadow-xl shadow-gray-125'>
+              <SheetHeader className='text-left p-6 pb-4 border-b'>
+                <SheetTitle>Adicionar Nova Área de Atução</SheetTitle>
+                <SheetDescription>
+                  Preencha os dados abaixo para cadastrar uma nova área.
+                </SheetDescription>
+              </SheetHeader>
+              <div className='flex-1 overflow-auto p-6'>
+                <AreaDeAruacaoForm onSubmit={handleFormSubmit}/>
+              </div>
+            </SheetContent>
+          </Sheet>
+      </header>
 
       <div className='shrink-0'>
         <DataTableToolbar table={table} />
