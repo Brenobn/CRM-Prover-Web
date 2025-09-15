c63abeb9423d7179a4913e9536811002b14dd0c5
Brenobn <brenobragabn@gmail.com>
2025-09-15 10:14:46 -0300

    Impoting useTheme and creating a const passing useTheme. Adjusting some styles too

--- src/pages/SiginIn.tsx
+++ src/pages/SiginIn.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom"
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 
+import { useTheme } from "../hooks/theme"
+import { PiSun, PiMoon } from "react-icons/pi"
+
 import { ShieldCheck } from "lucide-react"
 import { FaGoogle, FaGithub } from 'react-icons/fa'
 
@@ -10,17 +13,32 @@ export function SiginIn() {
   const { signIn } = useAuth()
   const navigate = useNavigate()
 
+  const { theme, toggleTheme } = useTheme()
+
   function handleLogin() {
     signIn()
     navigate('/')
   }
 
   return(
-    <div className="flex min-h-screen items-center justify-center bg-gray-75 dark:bg-gray-300 p-4">
+    <div className="relative flex min-h-screen items-center justify-center bg-gray-75 dark:bg-gray-150 p-4">
+
+      <Button
+        variant='ghost'
+        size='icon'
+        onClick={toggleTheme}
+        className="absolute top-6 right-6"
+      >
+        {theme === 'light' ? (
+          <PiMoon className="h-8 w-8" />
+        ) : (
+          <PiSun className="h-8 w-8 dark:text-white" />
+        )}
+      </Button>
       <div className="w-full max-w-md space-y-8">
 
         <div className="flex flex-col items-center text-center">
-          <ShieldCheck className="h-10 w-10 text-gray-150"/>
+          <ShieldCheck className="h-10 w-10 text-gray-150 dark:text-gray-75"/>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-blue-400 dark:text-white">
             Acesse sua conta
           </h1>
@@ -31,13 +49,16 @@ export function SiginIn() {
 
         <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-8">
           <div className="space-y-4">
-            <Button variant="outline" className="w-full">
-              <FaGoogle className="mr-2 h-4 w-4"/>
-              Continuar com Google
-            </Button>
-            <Button variant="outline" className="w-full">
-              <FaGithub className="mr-2 h-4 w-4"/>
-              Continuar com GitHub
+            <div className="space-y-2">
+              <label htmlFor="email" className="text-sm font-medium leading-none">Email</label>
+              <Input id="email" type="email" placeholder="seu@email.com" />
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="password" className="text-sm font-medium leading-none">Senha</label>
+              <Input id="password" type="password" placeholder="Sua senha" />
+            </div>
+            <Button onClick={handleLogin} className="w-full bg-blue-400 hover:bg-blue-500 transition-colors duration-300">
+              Entrar com Email
             </Button>
 
             <div className="relative my-6">
@@ -51,16 +72,13 @@ export function SiginIn() {
               </div>
             </div>
 
-            <div className="space-y-2">
-              <label htmlFor="email" className="text-sm font-medium leading-none">Email</label>
-              <Input id="email" type="email" placeholder="seu@email.com" />
-            </div>
-            <div className="space-y-2">
-              <label htmlFor="password" className="text-sm font-medium leading-none">Senha</label>
-              <Input id="password" type="password" placeholder="Sua senha" />
-            </div>
-            <Button onClick={handleLogin} className="w-full bg-blue-400 hover:bg-blue-500 transition-colors duration-300">
-              Entrar com Email
+            <Button variant="outline" className="w-full">
+              <FaGoogle className="mr-2 h-4 w-4"/>
+              Continuar com Google
+            </Button>
+            <Button variant="outline" className="w-full">
+              <FaGithub className="mr-2 h-4 w-4"/>
+              Continuar com GitHub
             </Button>
           </div>
         </div>
--- src/pages/SiginUp.tsx
+++ src/pages/SiginUp.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom"
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 
+import { useTheme } from "../hooks/theme"
+import { PiSun, PiMoon } from "react-icons/pi"
+
 import { ShieldCheck } from "lucide-react"
 import { FaGoogle, FaGithub } from 'react-icons/fa'
 
@@ -10,17 +13,32 @@ export function SiginUp() {
   const { signIn } = useAuth()
   const navigate = useNavigate()
 
+  const { theme, toggleTheme } = useTheme()
+
   function handleLogin() {
     signIn()
     navigate('/')
   }
 
   return(
-    <div className="flex min-h-screen items-center justify-center bg-gray-75 dark:bg-gray-300 p-4">
-      <div className="w-full max-w-md space-y-8">
+    <div className="relative flex min-h-screen items-center justify-center bg-gray-75 dark:bg-gray-150 transition-colors duration-300 p-4">
+
+      <Button
+        variant='ghost'
+        size='icon'
+        onClick={toggleTheme}
+        className="absolute top-6 right-6"
+      >
+        {theme === 'light' ? (
+          <PiMoon className="h-8 w-8" />
+        ) : (
+          <PiSun className="h-8 w-8 dark:text-white transition-colors duration-300" />
+        )}
+      </Button>
+      <div className="w-full max-w-md space-y-8 transition-colors duration-300">
 
         <div className="flex flex-col items-center text-center">
-          <ShieldCheck className="h-10 w-10 text-gray-125"/>
+          <ShieldCheck className="h-10 w-10 text-gray-150 dark:text-gray-75"/>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-blue-400 dark:text-white">
             Crie sua conta
           </h1>
@@ -31,26 +49,6 @@ export function SiginUp() {
 
         <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-8">
           <div className="space-y-4">
-            <Button variant="outline" className="w-full">
-              <FaGoogle className="mr-2 h-4 w-4"/>
-              Cadastrar com Google
-            </Button>
-            <Button variant="outline" className="w-full">
-              <FaGithub className="mr-2 h-4 w-4"/>
-              Cadastrar com GitHub
-            </Button>
-
-            <div className="relative my-6">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-card px-2 text-muted-foreground">
-                  Ou cadastre-se com email
-                </span>
-              </div>
-            </div>
-
             <div className="space-y-2">
               <label htmlFor="name" className="text-sm font-medium leading-none">Nome completo</label>
               <Input id="name" type="text" placeholder="Seu nome completo" />
@@ -63,9 +61,31 @@ export function SiginUp() {
               <label htmlFor="password" className="text-sm font-medium leading-none">Senha</label>
               <Input id="password" type="password" placeholder="Sua senha" />
             </div>
+
             <Button onClick={handleLogin} className="w-full bg-blue-400 hover:bg-blue-500 transition-colors duration-300">
              Continuar
             </Button>
+
+            <div className="relative my-6">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-card px-2 text-muted-foreground">
+                  Ou cadastre-se com
+                </span>
+              </div>
+            </div>
+
+            <Button variant="outline" className="w-full transition-colors duration-300">
+              <FaGoogle className="mr-2 h-4 w-4"/>
+              Cadastrar com Google
+            </Button>
+            <Button variant="outline" className="w-full transition-colors duration-300">
+              <FaGithub className="mr-2 h-4 w-4"/>
+              Cadastrar com GitHub
+            </Button>
+
           </div>
         </div>
 
