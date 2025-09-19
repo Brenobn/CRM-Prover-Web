8651de91ed01426f46bf3246ddca3160d7fdc297
Brenobn <brenobragabn@gmail.com>
2025-09-19 09:59:16 -0300

    Changing the style logic, using if and else conditions and passing isOpen as a boolean property

--- src/components/Sidebar/MainNavigation/NavItem.tsx
+++ src/components/Sidebar/MainNavigation/NavItem.tsx
@@ -5,7 +5,7 @@ import { tv } from 'tailwind-variants';
 const navItem = tv({
   slots: {
     trigger:
-      'w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-1 transition-colors duration-300 ease-in-out hover:text-gray-200 hover:bg-gray-75 dark:text-gray-75 dark:hover:bg-gray-100',
+      'w-full flex items-center px-3 py-2 rounded-lg gap-2 group cursor-pointer mb-1 transition-colors duration-300 ease-in-out hover:text-gray-200 hover:bg-gray-75 dark:hover:text-white dark:text-gray-75 dark:hover:bg-gray-100',
     mainIcon: 'h-5 w-5 text-gray-100 dark:text-gray-75',
   },
   variants: {
@@ -19,13 +19,14 @@ const navItem = tv({
 });
 
 export interface NavItemProps {
-  title: string;
-  icon: ElementType;
-  children?: ReactNode;
+  title: string
+  icon: ElementType
+  children?: ReactNode
+  isOpen: boolean
 }
 
-export function NavItem({ title, icon: Icon, children }: NavItemProps) {
-  const [isOpen, setIsOpen] = useState(false);
+export function NavItem({ title, icon: Icon, children, isOpen: isSidebarOpen }: NavItemProps) {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
   const hasChildren = children != null;
 
   const { trigger, mainIcon } = navItem({ hasChildren });
@@ -34,33 +35,35 @@ export function NavItem({ title, icon: Icon, children }: NavItemProps) {
     return (
       <a href="#" className={trigger()}>
         <Icon className={mainIcon()} />
-        <span>{title}</span>
+        {isSidebarOpen && <span className='text-sm font-medium'>{title}</span>}
       </a>
     );
   }
 
   return (
     <div>
-      <button onClick={() => setIsOpen(!isOpen)} className={trigger()}>
-        <span className="flex items-center gap-2">
+      <button 
+        onClick={() => { if (isSidebarOpen) setIsSubMenuOpen(!isSubMenuOpen); }} 
+        className={trigger()}
+      >
+        <span className="flex items-center gap-3">
           <Icon className={mainIcon()} />
-          <span className='text-sm font-medium text-gray-100 dark:text-white'>{title}</span>
+          {isSidebarOpen && <span className="text-sm font-medium">{title}</span>}
         </span>
-
-        <IoChevronBack size={16} className={`
-            text-gray-100 dark:text-gray-75
-            transition-transform duration-400 ease-in-out
-            ${isOpen ? '-rotate-90' : 'rotate-0'}
-          `} />
+        {hasChildren && isSidebarOpen && (
+          <IoChevronBack size={16} className={`transition-transform duration-300 ${isSubMenuOpen ? '-rotate-90' : 'rotate-0'}`} />
+        )}
       </button>
 
-      <div
-        className={`overflow-hidden transition-all duration-300 flex flex-col pl-10 space-y-1 ${
-          isOpen ? 'max-h-60' : 'max-h-0'
-        }`}
-      >
-        {children}
-      </div>
+      {isSidebarOpen && (
+        <div
+          className={`overflow-hidden transition-all duration-300 flex flex-col pl-10 space-y-1 ${
+            isSubMenuOpen ? 'max-h-60' : 'max-h-0'
+          }`}
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 }
