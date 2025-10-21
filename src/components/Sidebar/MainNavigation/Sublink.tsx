360c77a5e0153d04fc1ebb86b66f35db4fec0577
Brenobn <brenobragabn@gmail.com>
2025-10-21 13:46:12 -0300

    Adding whitespace-nowrap and truncate to the sublinks style

--- src/components/Sidebar/MainNavigation/Sublink.tsx
+++ src/components/Sidebar/MainNavigation/Sublink.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 import { tv } from "tailwind-variants";
 
 const sublinkStyles = tv({
-  base: 'px-3 py-2 rounded-lg cursor-pointer transition-colors duration-300 ease-in-out text-left text-gray-300 text-sm hover:text-gray-200 hover:bg-gray-75 dark:text-white dark:hover:bg-gray-100 dark:hover:text-white',
+  base: 'px-3 py-2 rounded-lg cursor-pointer transition-colors duration-300 ease-in-out text-left text-gray-300 text-sm hover:text-gray-200 hover:bg-gray-75 dark:text-white dark:hover:bg-gray-100 dark:hover:text-white whitespace-nowrap truncate',
   variants: {
     isActive: {
       true: 'bg-gray-75 dark:bg-gray-100 font-semibold',
