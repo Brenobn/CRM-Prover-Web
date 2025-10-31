07a9e500fd3630f103cfc5384e30e4a26c479401
Brenobn <brenobragabn@gmail.com>
2025-10-31 10:02:07 -0300

    Replacing the 'a' tag with the react-router Link.

--- src/components/Sidebar/MainNavigation/Sublink.tsx
+++ src/components/Sidebar/MainNavigation/Sublink.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { tv } from "tailwind-variants";
+import { Link } from "react-router-dom";
 
 const sublinkStyles = tv({
   base: 'px-3 py-2 rounded-lg cursor-pointer transition-colors duration-300 ease-in-out text-left text-gray-300 text-sm hover:text-gray-200 hover:bg-gray-75 dark:text-white dark:hover:bg-gray-100 dark:hover:text-white whitespace-nowrap truncate',
@@ -22,8 +23,8 @@ export interface SublinkProps {
 
 export function Sublink({ href, children, isActive }: SublinkProps) {
   return(
-    <a href={href} className={sublinkStyles({ isActive })}>
+    <Link to={href} className={sublinkStyles({ isActive })}>
       {children}
-    </a>
+    </Link>
   )
 }
